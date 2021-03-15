9807136fb1e6bc7cf8ad538373939e8fd1c35244
mfalling <mfalling@usf.edu>
2021-03-15 16:31:36 -0400

    Reorganizing script

--- smiley-face-gif.R
+++ smiley-face-gif.R
@@ -1,7 +1,9 @@
+# Create Mouth ------------------------------------------------------------
 
-
-
+# Empty df
 mouth <- data.frame()
+
+# Create a parabola, `y`, and imperatively draw each point.
 for (i in -20:10){
   x <- i
   y <- .1*x^2 + x + 10
@@ -11,24 +13,33 @@ for (i in -20:10){
 }
 
 
+# Prep for Eyes -----------------------------------------------------------
+
+# Set radius (how big the eyes are)
 radius <- 3
 
+# Set theta (amount of dots and angle)
 theta <- seq(from = 0, to = 5.5,  by = .5)
-xcoords <- cos(theta) * (radius) + 1
+
+# Set coordinates for circles (left and right eyes)
 ycoords <- sin(theta) * (radius * 2.5) + 50
+Lxcoords <- cos(theta) * (radius) + -10
+Rxcoords <- cos(theta) * (radius) + 1
 
-righteye <- data.frame()
-for (i in 1:12) {
-  coords <- cbind(xcoords[i], ycoords[i])
-  righteye <- rbind(righteye, coords)
-  points(righteye$V1, righteye$V2, xlim = c(-50,50), ylim = c(0,125))
-}
+# Create Eyes -------------------------------------------------------------
 
-xv <- cos(theta) * (radius) + -10
-
+# Draw the left eye
 lefteye <- data.frame()
 for (i in 1:12) {
-  coords <- cbind(xv[i], yv[i])
+  coords <- cbind(Lxcoords[i], ycoords[i])
   lefteye <- rbind(lefteye, coords)
   points(lefteye$V1, lefteye$V2, xlim = c(-50,50), ylim = c(0,125))
+}
+
+# Draw the right eye
+righteye <- data.frame()
+for (i in 1:12) {
+  coords <- cbind(Rxcoords[i], ycoords[i])
+  righteye <- rbind(righteye, coords)
+  points(righteye$V1, righteye$V2, xlim = c(-50,50), ylim = c(0,125))
 }
