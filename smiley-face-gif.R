ffed58fff5f913c949b5274de58be39c7b84c602
mfalling <mfalling@usf.edu>
2021-03-15 16:24:30 -0400

    Created Circles

--- smiley-face-gif.R
+++ smiley-face-gif.R
@@ -1,17 +1,34 @@
 
-x <- 5
-c <- 0
-a <- .1
-b <- 1
-y <- a*(x^2) + b*x + c
+
 
 mouth <- data.frame()
-for (i in -25:15){
+for (i in -20:10){
   x <- i
-  y <- a*x^2 + b*x + c
-  print(y)
+  y <- .1*x^2 + x + 10
   coords <- cbind(x, y)
   mouth <- rbind(mouth, coords)
   plot(mouth$x, mouth$y, xlim = c(-50,50), ylim = c(0,125))
 }
 
+
+radius <- 3
+
+theta <- seq(from = 0, to = 5.5,  by = .5)
+xcoords <- cos(theta) * (radius) + 1
+ycoords <- sin(theta) * (radius * 2.5) + 50
+
+righteye <- data.frame()
+for (i in 1:12) {
+  coords <- cbind(xcoords[i], ycoords[i])
+  righteye <- rbind(righteye, coords)
+  points(righteye$V1, righteye$V2, xlim = c(-50,50), ylim = c(0,125))
+}
+
+xv <- cos(theta) * (radius) + -10
+
+lefteye <- data.frame()
+for (i in 1:12) {
+  coords <- cbind(xv[i], yv[i])
+  lefteye <- rbind(lefteye, coords)
+  points(lefteye$V1, lefteye$V2, xlim = c(-50,50), ylim = c(0,125))
+}
