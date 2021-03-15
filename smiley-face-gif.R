ac05ce0138b901002d013d6bebb402ea9b869113
mfalling <mfalling@usf.edu>
2021-03-15 15:29:28 -0400

    Adjusting x^2 coefficient

--- smiley-face-gif.R
+++ smiley-face-gif.R
@@ -1,13 +1,12 @@
 
 x <- 5
 c <- 0
-a <- 1
+a <- .1
 b <- 1
 y <- a*(x^2) + b*x + c
+
 mouth <- data.frame()
-
-
-for (i in -10:10){
+for (i in -25:15){
   x <- i
   y <- a*x^2 + b*x + c
   print(y)
