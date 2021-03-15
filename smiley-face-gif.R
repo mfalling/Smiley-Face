a0b8517dc46dacaa931b3c1e5e295ac79e86063c
mfalling <mfalling@usf.edu>
2021-03-15 15:26:21 -0400

    Building Parabola

---
+++ smiley-face-gif.R
@@ -0,0 +1,18 @@
+
+x <- 5
+c <- 0
+a <- 1
+b <- 1
+y <- a*(x^2) + b*x + c
+mouth <- data.frame()
+
+
+for (i in -10:10){
+  x <- i
+  y <- a*x^2 + b*x + c
+  print(y)
+  coords <- cbind(x, y)
+  mouth <- rbind(mouth, coords)
+  plot(mouth$x, mouth$y, xlim = c(-50,50), ylim = c(0,125))
+}
+
